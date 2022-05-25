fd000af5f1470b492ad25fb6ef987bfc29b9b38e
ani0104 <annie.samajpati@gmail.com>
2022-05-25 18:26:45 -0400

    update backend auth.js

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const UserModel = require('../Models/UserModel')
 const router = express.Router();
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken')
 
+const JWT_SECRET = '@nisamajpati'
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -16,18 +19,29 @@ router.post('/createuser', [
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() })
         }
-        //check whether the user with this email exists already
         try {
+            //check whether the user with this email exists already
             let user = await UserModel.findOne({ email: req.body.email })
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exists" })
-            } // create a new user
+            } 
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt)
+            // create a new user
             user = await UserModel.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPass,
             })
-            res.json({ user })
+            const data ={
+                 user: {
+                     id: user.id
+                 }
+            }
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            // console.log(authtoken)
+            // res.json({ user })
+            res.json({authtoken})
 
             //catch errors
         } catch (error) {
